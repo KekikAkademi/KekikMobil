631c16cdfe981aa8a989568b708331bbcd805802
keyiflerolsun <keyiflerolsun@gmail.com>
2021-02-11 07:53:37 +0300

    🕊

---
+++ KekikMobil/_endpointler/giris_yap.py
@@ -0,0 +1,10 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+from KekikMobil import app, log_ver
+from flask import render_template, request
+
+@app.route('/giris_yap')
+def giris_yap():
+    log_ver(request)
+
+    return render_template('giris_yap.html')
